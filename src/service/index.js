36f73e8ec86b5e6ef4ccded83d4d12e36637eda0
Joao Pedro <betajpsg@gmail.com>
2020-06-16 20:28:06 -0300

    atualizando os dados que são enviados para o front
    enviando array para gráficos

--- src/service/index.js
+++ src/service/index.js
@@ -1,23 +1,16 @@
-//const $ = document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
 const url = 'https://brasil.io/api/dataset/covid19/caso_full/data?state=BA&city=Santo+Amaro';
 let mostCurrentData = {};
+let covidEvolutionData = [];
 
 window.addEventListener('load', async () => {
     let response =
         await fetch(url).then((res) => {
             res.json().then((data) => {
                 let result = data.results
-                mostCurrentData = {
-                    "date": result[0].date,
-                    "last_confirmed": result[0].last_available_confirmed,
-                    "new_confirmed": result[0].new_confirmed,
-                    "last_available_data": result[0].last_available_date,
-                    "last_available_death_rate": result[0].last_available_death_rate,
-                    "last_available_death": result[0].last_available_deaths,
-                }
-                testar(mostCurrentData)
-                //testar(data.results)
-                //console.log(data.results)
+
+                getMostRecenteDataAsJson(result)
+                getDataAsArray(result)
             })
         }).catch((err) => {
             console.log(err);
@@ -25,6 +18,24 @@ window.addEventListener('load', async () => {
 });
 
 
-function testar(varTeste){
-   console.log(varTeste)
+function getMostRecenteDataAsJson(result){
+    mostCurrentData = {
+        "date": result[0].date,
+        "last_confirmed": result[0].last_available_confirmed,
+        "new_confirmed": result[0].new_confirmed,
+        "last_available_data": result[0].last_available_date,
+        "last_available_death_rate": result[0].last_available_death_rate,
+        "last_available_death": result[0].last_available_deaths,
+    }
+    console.log(mostCurrentData)
+    return mostCurrentData
+}
+
+function getDataAsArray(result){
+    result.forEach(item => {
+        covidEvolutionData.push([item.date, item.last_available_confirmed])
+    });
+
+    console.log(covidEvolutionData)
+    return result
 }
