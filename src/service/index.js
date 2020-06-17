bfcd75ed0fdc3356ba94dfeda81d90f022d68527
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-06-17 18:18:45 -0300

    Atualização do site
    -Adicionado Gráfico e Tabela de casos
    -Design Reformulado
    -Alteração e criação de funções para a utilização do ChartJs

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,17 @@
 const $ = document.querySelector.bind(document);
 const url = 'https://brasil.io/api/dataset/covid19/caso_full/data?state=BA&city=Santo+Amaro';
 let mostCurrentData = {};
-let covidEvolutionData = [];
+var covidEvolutionDate= [];
+var covidEvolutionQtd = [];
+
+
+const contaminados = document.querySelector("#contaminados-value");
+const mortos = document.querySelector("#mortos-value");
+const percentual = document.querySelector("#percentual-value");
+const data = document.querySelector("#att-value");
+var ctx = document.getElementById('myChart').getContext('2d');
+const div = document.querySelector('#relatorio');
+
 
 window.addEventListener('load', async () => {
     let response =
@@ -11,6 +21,8 @@ window.addEventListener('load', async () => {
 
                 getMostRecenteDataAsJson(result)
                 getDataAsArray(result)
+                createGraph()
+                generateRelatoryData()
             })
         }).catch((err) => {
             console.log(err);
@@ -27,15 +39,97 @@ function getMostRecenteDataAsJson(result){
         "last_available_death_rate": result[0].last_available_death_rate,
         "last_available_death": result[0].last_available_deaths,
     }
+    contaminados.innerHTML = mostCurrentData.last_confirmed;
+    mortos.innerHTML = mostCurrentData.last_available_death;
+    percentual.innerHTML = mostCurrentData.last_available_death_rate + "%";
+
+    let date = mostCurrentData.date.split('-');
+    data.innerHTML = date[2] + "/" + date[1] + "/" + date[0];
     console.log(mostCurrentData)
     return mostCurrentData
 }
 
 function getDataAsArray(result){
     result.forEach(item => {
-        covidEvolutionData.push([item.date, item.last_available_confirmed])
+        let split = item.date.split('-')
+        covidEvolutionDate.push(split[2] + '/' + split[1] + '/' + split[0]);
+        covidEvolutionQtd.push(item.last_available_confirmed)
     });
 
-    console.log(covidEvolutionData)
+    
     return result
 }
+
+
+
+
+function createGraph(){
+
+    var date ={
+        labels: covidEvolutionDate,
+        datasets: [{
+            label: 'Quantidade de  contaminados:',
+            backgroundColor: "rgb(11,72,107)",
+            data: covidEvolutionQtd,
+           
+        }]
+    };
+    
+    var options = {
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            stacked: true,
+            gridLines: {
+              display: true,
+              color: "rgb(11,72,107,0.2)"
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              display: false
+            }
+          }]
+        }
+        
+    }
+    
+    
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: date,
+        options: options
+    });
+
+}
+
+function generateRelatoryData(){
+  let content = '<h1>Relatório:</h1>'
+   + '<table >' 
+      + '<tr>'
+      + '<th>Data</th>'
+      + '<th>Contaminados</th>' 
+      +'</tr>';
+
+  div.removeChild;
+
+  for(var i =0; i<covidEvolutionDate.length; i++) {
+      content += '<tr>' 
+      +'<td>' + covidEvolutionDate[i] + '</td>'
+      +'<td>' + covidEvolutionQtd[i] + '</td>'
+      + '</tr>'
+      
+      
+  }
+
+  content += '</table>';
+
+  div.innerHTML = content;
+
+
+}
+
+
+
+
+
